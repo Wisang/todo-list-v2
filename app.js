ef127346459612855f58935157887e5740709c74
wisang <wseom7@gmail.com>
2022-09-18 11:56:54 +0900

    read from db

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/todolistDB");
@@ -19,27 +21,39 @@ const itemsSchema = {
 
 const Item = mongoose.model('Item', itemsSchema);
 
-const webDev = new Item({name: "Web development"});
-const stepping = new Item({name: "Stepping"});
-const chinUp = new Item({name: "Chin up"});
+const webDev = new Item({
+  name: "Web development"
+});
+const stepping = new Item({
+  name: "Stepping"
+});
+const chinUp = new Item({
+  name: "Chin up"
+});
 
 const defaultItmes = [webDev, stepping, chinUp];
 
-Item.insertMany(defaultItmes, function(err) {
-  if(err) {
-    console.error(err);
-  } else {
-    console.log("Success");
-  }
-});
-
 app.get("/", function(req, res) {
-
-  res.render("list", {listTitle: "Today", newListItems: items});
+  Item.find({}, function(err, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItmes, function(err) {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log("Success");
+        }
+      });
+    }
+    res.render("list", {
+      listTitle: "Today",
+      newListItems: foundItems
+    });
+  });
+  // res.render("list", {listTitle: "Today", newListItems: items});
 
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
 
   const item = req.body.newItem;
 
@@ -52,11 +66,14 @@ app.post("/", function(req, res){
   }
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function(req, res) {
   res.render("about");
 });
 
